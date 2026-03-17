b4e03200bc948fb742b9c727103d9d79277e8bb2
vrui-create <vrui@gaming.tech>
2026-03-17 16:02:01 +0100

    Cook

--- Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
+++ Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
@@ -1,28 +1,39 @@
 using UnityEngine;
 using TMPro;
 using System;
+using Unity.VisualScripting;
+
 
 [Serializable]
 public class Tache_Layourt
 {
-    public int RR;
+    [Header("Inscription Tache")]
     [SerializeField] public GameObject[] AllObject;
 }
 
 public class SC_Tache : MonoBehaviour
 {
+    public Camera CM_Player;
     public Tache_Layourt[] TL;
+    public int Layourt;
     void Start()
     {
-        
+        if(CM_Player==null)CM_Player = GameObject.Find("CameraManager").GetComponent<Camera>();
     }
     void Update()
     {
-        
-    }
+        if(CM_Player != null)
+        {
+            Vector3 Camera = CM_Player.transform.position;
+            if(Camera.z/20 <0)
+            {
+                
+            }
+            else
+            {
 
-    public void SwitchLayourt(int Layourt)
-    {
-        
+            }
+            Debug.Log($"Position Camera{Camera.z}");
+        }
     }
 }
