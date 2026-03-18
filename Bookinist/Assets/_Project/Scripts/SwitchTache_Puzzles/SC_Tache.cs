4491d91178beab4ab1931f710bb95a87debbfacb
vrui-create <vrui@gaming.tech>
2026-03-18 10:00:40 +0100

    [Update] UI Panel Tache

--- Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
+++ Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
@@ -9,19 +9,20 @@ using System.Runtime.CompilerServices; // Permet d'utiliser une Coroutine
 public class Tache_Layourt
 {
     [Header("Inscription Tache")]
-    [SerializeField] public GameObject[] AllObject;
+    [SerializeField] public string[] Objectif;
 }
 
 public class SC_Tache : MonoBehaviour
 {
     [Header("Variable Utiliser pour le Chronomêtre")]
     [SerializeField] public TextMeshProUGUI Text_Chronom;
-    [SerializeField] public TextMeshProUGUI Text_Layeur;
+    [SerializeField] public TextMeshProUGUI Text_Objectif;
     public int totalSeconds;
+
     [Header("Autre")]
     public Camera CM_Player;
-    public Tache_Layourt[] TL;
-    public int Layourt;
+    public Tache_Layourt[] Tache_Dans_Ce_Layeur;
+    private int WaitCondition;
 
 
     void Start()
@@ -31,16 +32,35 @@ public class SC_Tache : MonoBehaviour
     }
     void Update()
     {
-        if(CM_Player != null&& Text_Layeur!=null)
+        //Ce code consiste a vêrifier le layeur du joueur en fonction de sa position axe z.
+        //Text_Objectif.text = $"Layeur: {Mathf.Round(CM_Player.transform.position.z / 20)}";
+        if(CM_Player != null&& Text_Objectif != null && WaitCondition != (int)Mathf.Round(CM_Player.transform.position.z)+1)
         {
-            Text_Layeur.text = $"Layeur: {Mathf.Round(CM_Player.transform.position.z / 20)}";
+            WaitCondition = (int)Mathf.Round(CM_Player.transform.position.z / 20)+1;
+            string Message = "";
+            switch (WaitCondition)
+            {
+                case 0:
+                    int i = 0;
+                    while (i< Tache_Dans_Ce_Layeur[WaitCondition].Objectif.Length)
+                    {
+                        Message += "\n";
+                        Message += Tache_Dans_Ce_Layeur[WaitCondition].Objectif[i];
+                        i++;
+                    }
+
+                    break;
+            }
+            
+
+            //Text_Objectif.text = $"{Tache_Dans_Ce_Layeur[RR].Objectif[0]}";
         }
     }
 
-    public void Panel_Tache(GameObject GO) // Ce code permet uniquement de cacher ou non le panel des Taches
+    /*public void Panel_Tache(GameObject GO) // Ce code permet uniquement de cacher ou non le panel des Taches
     {
         if(GO!=null) GO.SetActive(!GO.activeSelf);
-    }
+    }*/
 
     IEnumerator Chronometre()
     {
