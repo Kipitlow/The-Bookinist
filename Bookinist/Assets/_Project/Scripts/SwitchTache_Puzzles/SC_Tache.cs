caf2e06b4a4cdf0e9e240419179e0b2e68e69862
vrui-create <vrui@gaming.tech>
2026-03-18 02:08:48 +0100

    [Add]Chronomêtre activer

--- Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
+++ Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using TMPro;
 using System;
-using Unity.VisualScripting;
+using System.Collections; // Permet d'utiliser une Coroutine
 
 
 [Serializable]
@@ -13,16 +13,23 @@ public class Tache_Layourt
 
 public class SC_Tache : MonoBehaviour
 {
+    [Header("Variable Utiliser pour le Chronomêtre")]
+    [SerializeField] public TextMeshProUGUI Text_Chronom;
+    public int totalSeconds;
+    [Header("Autre")]
     public Camera CM_Player;
     public Tache_Layourt[] TL;
     public int Layourt;
+
+
     void Start()
     {
         if(CM_Player==null)CM_Player = GameObject.Find("CameraManager").GetComponent<Camera>();
+        StartCoroutine("Chronometre"); //Permet de lancer la coroutine;
     }
     void Update()
     {
-        if(CM_Player != null)
+        /*if(CM_Player != null)
         {
             Vector3 Camera = CM_Player.transform.position;
             if(Camera.z/20 <0)
@@ -34,6 +41,27 @@ public class SC_Tache : MonoBehaviour
 
             }
             Debug.Log($"Position Camera{Camera.z}");
+        }*/
+    }
+
+    public void Panel_Tache(GameObject GO) // Ce code permet uniquement de cacher ou non le panel des Taches
+    {
+        if(GO!=null) GO.SetActive(!GO.activeSelf);
+    }
+
+    IEnumerator Chronometre()
+    {
+        
+        if(totalSeconds-1 >=1)
+        {
+            totalSeconds-=1;
+            Text_Chronom.text = $"Time: {totalSeconds/60}min {totalSeconds%60}Sec";
+            yield return new WaitForSeconds(1);
+            StartCoroutine("Chronometre");
+        }
+        else
+        {
+            StopCoroutine("Chronometre");
         }
     }
 }
