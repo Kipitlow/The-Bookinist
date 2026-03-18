aeb4be4e8cd63c8eddce9c8c2e50e162f7f24bd6
vrui-create <vrui@gaming.tech>
2026-03-18 22:19:27 +0100

    [Update] Canva
    -mise a jours le panel des tache on fonction du layeur du joueur.
    
    -Ajoute fonctionnaliter plus il y a des égnigme plus les prefable spawn sur l'axe y

--- Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
+++ Bookinist/Assets/_Project/Scripts/SwitchTache_Puzzles/SC_Tache.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
-using TMPro;
 using System;
 using System.Collections;
 using System.Runtime.CompilerServices; // Permet d'utiliser une Coroutine
+using TMPro;
+using UnityEngine;
+using System.Collections.Generic; // Permet de faire un tableau
 
 
 [Serializable]
@@ -16,8 +17,14 @@ public class SC_Tache : MonoBehaviour
 {
     [Header("Variable Utiliser pour le Chronomêtre")]
     [SerializeField] public TextMeshProUGUI Text_Chronom;
-    [SerializeField] public TextMeshProUGUI Text_Objectif;
+    [SerializeField] public TextMeshProUGUI Text_Objectif; //////Objectif
     public int totalSeconds;
+
+    [Header("Prefable")]
+    [SerializeField] public GameObject PrefableTache;
+    [SerializeField] public Transform Target_Parent_Prefable;
+    private List<GameObject> List_Temporair_Tache = new List<GameObject>();
+    //private GameObject[] List_Temporair_Tache;
 
     [Header("Autre")]
     public Camera CM_Player;
@@ -29,39 +36,49 @@ public class SC_Tache : MonoBehaviour
     {
         if(CM_Player==null)CM_Player = GameObject.Find("CameraManager").GetComponent<Camera>();
         StartCoroutine("Chronometre"); //Permet de lancer la coroutine;
+        
     }
     void Update()
     {
-        //Ce code consiste a vêrifier le layeur du joueur en fonction de sa position axe z.
-        //Text_Objectif.text = $"Layeur: {Mathf.Round(CM_Player.transform.position.z / 20)}";
-        if(CM_Player != null&& Text_Objectif != null && WaitCondition != (int)Mathf.Round(CM_Player.transform.position.z)+1)
-        {
-            WaitCondition = (int)Mathf.Round(CM_Player.transform.position.z / 20)+1;
-            string Message = "";
-            switch (WaitCondition)
-            {
-                case 0:
-                    int i = 0;
-                    while (i< Tache_Dans_Ce_Layeur[WaitCondition].Objectif.Length)
-                    {
-                        Message += "\n";
-                        Message += Tache_Dans_Ce_Layeur[WaitCondition].Objectif[i];
-                        i++;
-                    }
 
-                    break;
-            }
-            
+        if (CM_Player != null && WaitCondition != (int)Mathf.Round(CM_Player.transform.position.z) + 1)  //Ce code consiste a vêrifier le layeur du joueur en fonction de sa position axe z et enfin de le terminer quand un changement est fait.     //&& Text_Objectif != null
+        {//Text_Objectif.text = $"Layeur: {Mathf.Round(CM_Player.transform.position.z / 20)}";
 
-            //Text_Objectif.text = $"{Tache_Dans_Ce_Layeur[RR].Objectif[0]}";
+            WaitCondition = (int)Mathf.Round(CM_Player.transform.position.z / 20) + 1;
+            Change_Tach_List();
         }
+
     }
 
-    /*public void Panel_Tache(GameObject GO) // Ce code permet uniquement de cacher ou non le panel des Taches
+    void Change_Tach_List()
     {
-        if(GO!=null) GO.SetActive(!GO.activeSelf);
-    }*/
+        //CodePermettant De supprimer tous les taches present dans la list.
+        foreach (GameObject obj in List_Temporair_Tache)
+        {
+            if(obj!=null)Destroy(obj);
+        }
+        List_Temporair_Tache.Clear(); //On n'oublie pas de clear si on veux liberais de la place
 
+        
+        if (PrefableTache != null && Target_Parent_Prefable != null)
+        {
+            int Repeat = 0;
+            foreach (string iii in Tache_Dans_Ce_Layeur[WaitCondition].Objectif)
+            {
+                
+                GameObject AA = Instantiate(PrefableTache, Target_Parent_Prefable);
+                Vector3 Pos = AA.transform.position;
+                Pos.y = Pos.y - 100* Repeat;
+                AA.transform.position = Pos;
+                
+                
+                TextMeshProUGUI TT = AA.GetComponentInChildren<TextMeshProUGUI>();
+                TT.text = iii;
+                List_Temporair_Tache.Add(AA);
+                Repeat += 1;
+            }
+        }
+    }
     IEnumerator Chronometre()
     {
         float Position_x;
