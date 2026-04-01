1f87fb1e6ee6130e862eb2292ee2d0f643e574e2
vrui-create <vrui@gaming.tech>
2026-04-01 03:50:32 +0200

    [Add] Save Test Json

--- Bookinist/Assets/_Project/Scripts/CurrentManager/SC_Gestion_Energie.cs
+++ Bookinist/Assets/_Project/Scripts/CurrentManager/SC_Gestion_Energie.cs
@@ -1,37 +1,53 @@
 using System;
 using System.Collections;
+using System.IO;
 using TMPro;
 using UnityEngine;
+
+[Serializable]
+public class Saving_Json
+{
+    public int Energie { set; get; }
+    public string Last_Time;
+}
 
 public class SC_Gestion_Energie : MonoBehaviour
 {
     [Header("Time")]
-    private int Max_Energie= 5;
-    private int Energie = 0;
-    private float Current = 5;
-
-
-
-    DateTime Last_Time;
-    TimeSpan Resultat;
     private TextMeshProUGUI T_Horloge;
+    private int Max_Energie= 50;
+    private int Energie = 0;
+    private float Current = 2; //<-- cette variable est utiliser on tant que delay de recharge energie
+    TimeSpan Resultat;
+    //DateTime Last_Time;
+
+    [Header("Saving Json")]
+    private string NamePath;
+    private Saving_Json SaveData = new Saving_Json();
+
+    private void Awake()
+    {
+        NamePath = Path.Combine(Application.persistentDataPath, "energie.json");
+    }
+
 
     void Start()
     {
         T_Horloge = GameObject.Find("T_Horloge").GetComponent<TextMeshProUGUI>();
-        if (T_Horloge != null)
+        /*if (T_Horloge != null)
         {
-            //StartCoroutine(Time_Energie());
-        }
-        Last_Time = DateTime.Now;
+            Last_Time = DateTime.Now;
+            StartCoroutine(Time_Energie());
+        }*/
+        
     }
 
-    IEnumerator Time_Energie()
+    /*IEnumerator Time_Energie()
     {
         while (true)
         {
 
-            Resultat = Last_Time - DateTime.Now;
+            Resultat = DateTime.Now - Last_Time;
             int point_Energie = (int)Resultat.TotalSeconds / (int)Current;
             T_Horloge.text = $"Energie: {Mathf.Abs(point_Energie)}";
             print("1");
@@ -41,13 +57,47 @@ public class SC_Gestion_Energie : MonoBehaviour
             //T_Horloge.text = $"Time{Last_Time.ToString("HH:mm:ss")}, Calcule ";
             yield return new WaitForSeconds(1);
         }
-    }
+    }*/
 
-    public void BBBBBB()
+    
+    public void Loading()
     {
-        Resultat = Last_Time - DateTime.Now;
+        if(File.Exists(NamePath))
+        {
+            print(SaveData.Last_Time);
+            string json_DataPath = File.ReadAllText(NamePath);
+            SaveData = JsonUtility.FromJson<Saving_Json>(json_DataPath);
+            print(SaveData.Last_Time);
+            DateTime last_Timer = DateTime.Parse(SaveData.Last_Time);
+
+            Resultat = DateTime.Now - last_Timer;
+
+            Affiche_Energie();
+        }
+        else
+        {
+            Debug.LogError("Pas SauveGarde");
+        }
+    }
+    public void Affiche_Energie()
+    {
+        
         int point_Energie = (int)Resultat.TotalSeconds / (int)Current;
         T_Horloge.text = $"Energie: {Mathf.Abs(point_Energie)}";
+    }
+
+    public void Saving()
+    {
+
+        SaveData.Last_Time = DateTime.Now.ToString();
+        print(SaveData.Last_Time);
+        string JsonTo = JsonUtility.ToJson(SaveData);
+        File.WriteAllText(NamePath, JsonTo);
+    }
+
+    public void Deleting()
+    {
+        if (File.Exists(NamePath)) File.Delete(NamePath);
     }
 
 }
