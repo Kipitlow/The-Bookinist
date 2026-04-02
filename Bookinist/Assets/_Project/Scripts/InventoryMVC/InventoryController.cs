4a3017243632cd38b27dc32d421061c4091c8953
Amelia <147034763+DivineVoices@users.noreply.github.com>
2026-04-02 10:05:01 +0200

    [FIX] Inventory Fix

--- Bookinist/Assets/_Project/Scripts/InventoryMVC/InventoryController.cs
+++ Bookinist/Assets/_Project/Scripts/InventoryMVC/InventoryController.cs
@@ -34,6 +34,7 @@ public class InventoryController : MonoBehaviour
     {
         _inventoryModel.RemoveItem(ItemToAdd);
         UpdateInventory();
+        activeItem = null;
     }
 
     public void UpdateInventory()
