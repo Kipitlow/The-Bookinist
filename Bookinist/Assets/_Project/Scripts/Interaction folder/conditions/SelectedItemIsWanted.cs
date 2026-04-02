a45e41ffd9884659577668972d677d858e828d95
poplouf <paulferran102004@gmail.com>
2026-04-02 10:04:39 +0200

    [EDIT] clean + fix selected item

--- Bookinist/Assets/_Project/Scripts/Interaction folder/conditions/SelectedItemIsWanted.cs
+++ Bookinist/Assets/_Project/Scripts/Interaction folder/conditions/SelectedItemIsWanted.cs
@@ -5,6 +5,7 @@ public class SelectedItemIsWanted : MonoBehaviour
     [SerializeField] private InventoryController _inventoryController;
     public bool IsCorrectObject(Item objName)
     {
+        if (objName == null) return false;
         if (_inventoryController.activeItem.itemName == objName.itemName)
         {
             return true;
