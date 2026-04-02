141c13cfae35a33efcf01e4c29f8c16424a31316
vatea_RUI <vrui@gaming.tech>
2026-04-02 10:06:29 +0200

    [Update]
    Correction Json pour la régéner d'énergie ou plume

--- Bookinist/Assets/_Project/Scripts/CurrentManager/SC_Gestion_Energie.cs
+++ Bookinist/Assets/_Project/Scripts/CurrentManager/SC_Gestion_Energie.cs
@@ -5,10 +5,10 @@ using TMPro;
 using UnityEditor.Overlays;
 using UnityEngine;
 
-[Serializable]
+[System.Serializable]
 public class Saving_Json
 {
-    public int Energie { set; get; }
+    public int Energie;
     public string Last_Time;
 }
 
@@ -18,10 +18,14 @@ public class SC_Gestion_Energie : MonoBehaviour
     private TextMeshProUGUI T_Energie;
     private TextMeshProUGUI T_Horloge;
     public int Max_Energie;
+    public int Plume_Game;
     public float Current = 5; //<-- cette variable est utiliser on tant que delay de recharge energie
     float Temps_Restant;
     TimeSpan Resultat;
-    //DateTime Last_Time;
+
+    [Header("Text_Saving")]
+    public TextMeshProUGUI T_EnergieSaving;
+    public TextMeshProUGUI T_Plume;
 
     [Header("Saving Json")]
     private string NamePath;
@@ -29,7 +33,7 @@ public class SC_Gestion_Energie : MonoBehaviour
 
     private void Awake()
     {
-        NamePath = Path.Combine(Application.persistentDataPath, "energie.json");
+        NamePath = Application.persistentDataPath+ "/energie.json";
     }
 
 
@@ -37,12 +41,7 @@ public class SC_Gestion_Energie : MonoBehaviour
     {
         T_Energie = GameObject.Find("T_Energie").GetComponent<TextMeshProUGUI>();
         T_Horloge = GameObject.Find("T_Horloge").GetComponent<TextMeshProUGUI>();
-        Loading();
-        /*if (T_Horloge != null)
-        {
-            Last_Time = DateTime.Now;
-            StartCoroutine(Time_Energie());
-        }*/
+        Invoke("Loading", 0.5f);
 
     }
     private void Update()
@@ -56,11 +55,11 @@ public class SC_Gestion_Energie : MonoBehaviour
         {
             yield return new WaitForSeconds(Mathf.Abs(Temps_Restant - Current));
         }
-        while (SaveData.Energie < Max_Energie)
+        while (Plume_Game < Max_Energie)
         {
             print($"waitA1:{Current}");
-            SaveData.Energie += 1;
-            T_Energie.text = $"Energie: {SaveData.Energie}";
+            Plume_Game += 1;
+            T_Energie.text = $"Energie: {Plume_Game}";
             yield return new WaitForSeconds(Current);
         }
     }
@@ -73,46 +72,56 @@ public class SC_Gestion_Energie : MonoBehaviour
 
             string json_DataPath = File.ReadAllText(NamePath);
             SaveData = JsonUtility.FromJson<Saving_Json>(json_DataPath);
+
             DateTime last_Timer = DateTime.Parse(SaveData.Last_Time);
 
-            Resultat = DateTime.Now - last_Timer;
+            T_Plume.text = $"Plume_Game: {Plume_Game}";
+            T_EnergieSaving.text = $"Energie Savegarde: {SaveData.Energie}";
+
+
+            Resultat = DateTime.Now - last_Timer; //Code consite a prendre 2 type datatime (old/new) et le resultat c'est l'intervale entre les 2 time
+            Plume_Game += SaveData.Energie; // on setup cette variable depuis la sauvegarde
 
             Affiche_Energie();
             StartCoroutine(Time_Energie());
         }
         else
         {
-            Debug.LogError("Pas SauveGarde");
+            Debug.LogWarning("Pas SauveGarde");
         }
     }
     public void Affiche_Energie()
     {
-        
-        int totalSeconds = (int)Resultat.TotalSeconds;
+        int totalSeconds = (int)Resultat.TotalSeconds; //ce code consister a prendre
         int point_Energie = totalSeconds / (int)Current;
         Temps_Restant = totalSeconds % (int)Current;
-        print($"Energie Save: {SaveData.Energie}");
-        if (SaveData.Energie + point_Energie >= Max_Energie)
+
+
+        
+        if (Plume_Game + point_Energie >= Max_Energie)
         {
-            SaveData.Energie = Max_Energie;
+            Plume_Game = Max_Energie;
         }
         else
         {
-            SaveData.Energie += point_Energie;
+            Plume_Game += point_Energie;
         }
-        T_Horloge.text = $"Energie: {Mathf.Abs(SaveData.Energie)}";
+        T_Plume.text = $"Plume_Game: {Plume_Game}";
+        T_Horloge.text = $"Energie: {Mathf.Abs(Plume_Game)}";
     }
 
 
     private void OnApplicationQuit()
     {
-        Saving();
+        //Saving();
     }
 
     public void Saving()
     {
         SaveData.Last_Time = DateTime.Now.ToString();
-        print(SaveData.Last_Time);
+        SaveData.Energie = Plume_Game;
+
+
         string JsonTo = JsonUtility.ToJson(SaveData);
         File.WriteAllText(NamePath, JsonTo);
     }
