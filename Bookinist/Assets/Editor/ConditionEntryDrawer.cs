51e2cf67bb2b6c0e2112b9ad0cc4962ad6f9ffb5
poplouf <paulferran102004@gmail.com>
2026-04-02 16:15:14 +0200

    [EDIT] interaction

--- Bookinist/Assets/Editor/ConditionEntryDrawer.cs
+++ Bookinist/Assets/Editor/ConditionEntryDrawer.cs
@@ -36,6 +36,10 @@ public class ConditionEntryDrawer : PropertyDrawer
             case ConditionType.HasDialogueStarted:
                 lines += 1;
                 break;
+
+            case ConditionType.HasMoved:
+                lines += 3;
+                break;
         }
 
         return lines * (EditorGUIUtility.singleLineHeight + 2f);
@@ -92,6 +96,14 @@ public class ConditionEntryDrawer : PropertyDrawer
             case ConditionType.HasDialogueStarted:
                 EditorGUI.PropertyField(r, property.FindPropertyRelative("npcTalker"));
                 r.y += h + s;
+                break;
+
+            case ConditionType.HasMoved:
+                EditorGUI.PropertyField(r, property.FindPropertyRelative("Move"));
+                r.y += h + s;
+                EditorGUI.PropertyField(r, property.FindPropertyRelative("HasMoved"));
+                r.y += h + s;
+                EditorGUI.PropertyField(r, property.FindPropertyRelative("HowManyTimes"));
                 break;
         }
 
--- Bookinist/Assets/_Project/Scripts/Interaction folder/action/MoveOject.cs
+++ Bookinist/Assets/_Project/Scripts/Interaction folder/action/MoveOject.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MoveObject : MonoBehaviour
 {
@@ -6,6 +7,9 @@ public class MoveObject : MonoBehaviour
     #region Variables
     [Header("Movement")]
     [SerializeField] private float smoothTime = 0.2f;
+
+    private bool _hasMoved = false; 
+    private int _hasMovedThisManyTime; 
 
     private Vector3 _targetPosition;
     private Vector3 _velocity;
@@ -25,7 +29,21 @@ public class MoveObject : MonoBehaviour
 
     public void Move(float offsetX, float offsetY)
     {
+        Debug.Log("Move : " + _hasMoved);
         _targetPosition += new Vector3(offsetX, offsetY, 0f);
+        _hasMoved = true;
+        _hasMovedThisManyTime++;
+    }
+    #endregion
+
+    #region Check
+
+    public bool HasMoved(bool hasMoved, int HowManytime)
+    {
+        if (hasMoved)
+            return _hasMovedThisManyTime >= HowManytime;
+        else
+            return !_hasMoved;
     }
     #endregion
 }
