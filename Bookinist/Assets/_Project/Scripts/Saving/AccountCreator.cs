fac6e286c20159d4db191e76c604cd70a8dc4c78
Amelia <147034763+DivineVoices@users.noreply.github.com>
2026-04-13 13:04:01 +0200

    [ADD] File Deletion

--- Bookinist/Assets/_Project/Scripts/Saving/AccountCreator.cs
+++ Bookinist/Assets/_Project/Scripts/Saving/AccountCreator.cs
@@ -1,14 +1,15 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class AccountCreator : MonoBehaviour
 {
-    //[SerializeField] private InputField _usernameInput;
+    [SerializeField] private TMP_InputField _usernameInput;
 
-    public void CreateAccount(string username)
+    public void CreateAccount()
     {
         SaveSystem.instance.Save();
-        SaveService.instance.ChangeName(username);
+        SaveService.instance.ChangeName(_usernameInput.text);
         SaveSystem.instance.Save();
     }
 
