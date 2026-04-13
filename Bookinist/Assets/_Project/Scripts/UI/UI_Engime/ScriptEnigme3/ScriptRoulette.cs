53aa6d339af15823fc4b70000792f37334eacbba
vatea_RUI <vrui@gaming.tech>
2026-04-13 15:05:08 +0200

    Update
    Ajoute un sprite Sphere + ScriptRoulette, dans la 3émeScene, (Voir Taha d'enigme de la roulette honkai star rail)

---
+++ Bookinist/Assets/_Project/Scripts/UI/UI_Engime/ScriptEnigme3/ScriptRoulette.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public GameObject premierRoulette;
+    void Start()
+    {
+        //_selfSprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+    }
+}
