8272cbe918ce1217f68ada11d304afeccca89fc8
vatea_RUI <vrui@gaming.tech>
2026-04-14 17:28:48 +0200

    [Update] Scene3.
    add script roulette demande a taha pour plus explication sur la roulette de honkai star rail

--- Bookinist/Assets/_Project/Scripts/UI/UI_Engime/ScriptEnigme3/ScriptRoulette.cs
+++ Bookinist/Assets/_Project/Scripts/UI/UI_Engime/ScriptEnigme3/ScriptRoulette.cs
@@ -1,15 +1,113 @@
+using System.Collections;
 using UnityEngine;
 
 public class Script : MonoBehaviour
 {
-    public GameObject premierRoulette;
-    void Start()
+    [SerializeField] private GameObject _oneCercle;
+    [SerializeField] private GameObject _twoCercle;
+    [SerializeField] private GameObject _treeCercle;
+
+    [SerializeField] private float _second1Coroutine;
+    [SerializeField] private float _second2Coroutine;
+    [SerializeField] private float _second3Coroutine;
+
+    private bool _boolOneCoroutine;
+    private bool _boolTwoCoroutine;
+    private bool _boolThreeCoroutine;
+    private void Start()
     {
-        //_selfSprite = GetComponent<SpriteRenderer>();
+        StartCoroutine(_premierCercle());
+        StartCoroutine(_deuxiémeCercle());
+        StartCoroutine(_troisiémeCercle());
     }
 
-    void Update()
+    public void StartcouroutineRotation(int RR)
     {
-        
+        switch(RR)
+        {
+            case 0:
+                StartCoroutine(_premierCercle());
+                _boolOneCoroutine = false;
+                if (_boolOneCoroutine == true)
+                {
+                    
+                }
+                else
+                {
+                    StopCoroutine(_premierCercle());
+                    _boolOneCoroutine = true;
+                }
+                    break;
+            case 1:
+                if (_boolTwoCoroutine == true)
+                {
+                    StartCoroutine(_deuxiémeCercle());
+                    _boolTwoCoroutine = false;
+                }
+                else
+                {
+                    StopCoroutine(_deuxiémeCercle());
+                    _boolTwoCoroutine = true;
+                }
+                break;
+            case 2:
+                if (_boolThreeCoroutine == true)
+                {
+                    StartCoroutine(_troisiémeCercle());
+                }
+                else
+                {
+                    StopCoroutine(_troisiémeCercle());
+                }
+                break;
+        }
+    }
+    public void StopcouroutineRotation(int RR)
+    {
+        switch(RR)
+        {
+            case 0:
+                StopCoroutine(_premierCercle());
+                _boolOneCoroutine = true;
+                break;
+            case 1:
+                StopCoroutine(_deuxiémeCercle());
+                _boolTwoCoroutine = true;
+                break;
+            case 2:
+                if (_boolThreeCoroutine == true)
+                {
+                    StartCoroutine(_troisiémeCercle());
+                }
+                else
+                {
+                    StopCoroutine(_troisiémeCercle());
+                }
+                break;
+        }
+    }
+    IEnumerator _premierCercle()
+    {
+        while (_boolOneCoroutine)
+        {
+            _oneCercle.transform.Rotate(0.0f, 0.0f, _second1Coroutine * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    IEnumerator _deuxiémeCercle()
+    {
+        while (_boolTwoCoroutine)
+        {
+            _twoCercle.transform.Rotate(0.0f, 0.0f, _second2Coroutine * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+    IEnumerator _troisiémeCercle()
+    {
+        while (_boolThreeCoroutine)
+        {
+            _treeCercle.transform.Rotate(0.0f, 0.0f, _second3Coroutine * Time.deltaTime);
+            yield return new WaitForSeconds(0.01f);
+        }
     }
 }
