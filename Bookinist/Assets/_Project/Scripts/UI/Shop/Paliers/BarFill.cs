9380f2160e4351b5576b24851a81874e9bd3bd76
Amelia <147034763+DivineVoices@users.noreply.github.com>
2026-04-14 16:41:19 +0200

    [ADD] Palier bars done
    C'est tout
    Scene : Paliers

---
+++ Bookinist/Assets/_Project/Scripts/UI/Shop/Paliers/BarFill.cs
@@ -0,0 +1,22 @@
+using TMPro;
+using UnityEngine;
+
+public class BarFill : MonoBehaviour
+{
+    public float Width, Height, curXP, maxXP;
+
+    [SerializeField]
+    private RectTransform FillBar;
+
+    public void RefreshBarUI()
+    {
+
+        float newHeight = (curXP / maxXP) * Height;
+        FillBar.sizeDelta = new Vector2(Width, newHeight);
+    }
+
+    public void ModifCur(int newVal)
+    {
+        curXP = Mathf.Clamp(curXP += newVal, 0, maxXP);
+    }
+}
