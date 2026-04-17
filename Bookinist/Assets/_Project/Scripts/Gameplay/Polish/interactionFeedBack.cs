f208da5de90ea7d17b8b6b5c9d487feeb7873756
poplouf <pferran@gaming.tech>
2026-04-17 10:46:56 +0200

    [EDIT] fixes
    on peut recuperer la gemme, le collier, la flute est sur la table a nouveau collier de fleur sur l'enigme 2 est posable

---
+++ Bookinist/Assets/_Project/Scripts/Gameplay/Polish/interactionFeedBack.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class interactionFeedBack : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
