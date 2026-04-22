a0e56d0410be72130bea5f32b21cb43815630bb6
vatea_RUI <vrui@gaming.tech>
2026-04-22 16:22:29 +0200

    [En cours the fixation ethant]

--- Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UE_Enigme_2/ScriptChangeEau.cs
+++ Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UE_Enigme_2/ScriptChangeEau.cs
@@ -5,26 +5,23 @@ public class ScriptChangeEau : MonoBehaviour
 {
     private InteractionRunner _scriptActivation;
     private BoxCollider boxCollider;
-    private SpriteRenderer _selfSprite;
-    private int _nombreNuage;
+    private Material _material;
+    //private SpriteRenderer _selfSprite;
+    //private int _nombreNuage;
     private float _lerpColorFloat;
 
     void Start()
     {
-        _selfSprite = GetComponent<SpriteRenderer>();
+        //_selfSprite = GetComponent<SpriteRenderer>();
+        //_nombreNuage = 0;
+        _material = GetComponent<Material>();
         _scriptActivation = GetComponent<InteractionRunner>();
         boxCollider= GetComponent<BoxCollider>();
         Invoke("_DisableInteractionRunner", 0.1f);
-        _nombreNuage = 0;
         _lerpColorFloat = 0;
 
     }
 
-    /*private void _DisableInteractionRunner()
-    {
-        _scriptActivation.enabled = false;
-        boxCollider.enabled = false;
-    }*/
 
     public void ChangeColor()
     {
@@ -37,11 +34,15 @@ public class ScriptChangeEau : MonoBehaviour
 
         Color _colorOrigine = new Color(140.0f/ 255.0f,166.0f/255.0f, 163.0f / 255.0f, 1f);
         Color _colorChange = new Color(230.0f / 255.0f, 255.0f / 255.0f, 168.0f/255.0f, 1f);
+        Color _changingShadering;
         while (_lerpColorFloat < 1)
         {
             _lerpColorFloat+= 0.1f;
             //_selfSprite.color = Color.Lerp(Color.white, Color.yellow, _lerpColorFloat);
-            _selfSprite.color = Color.Lerp(_colorOrigine, _colorChange, _lerpColorFloat);
+            //_selfSprite.color = Color.Lerp(_colorOrigine, _colorChange, _lerpColorFloat);
+            _changingShadering = Color.Lerp(_colorOrigine, _colorChange, _lerpColorFloat);
+            _material.SetColor("BaseColor", _changingShadering);
+            Debug.Log("<color=green> Success</color>" + "ColorMaterial: " + _changingShadering);
             yield return new WaitForSeconds(0.1f);
         }
         _scriptActivation.enabled= true;
