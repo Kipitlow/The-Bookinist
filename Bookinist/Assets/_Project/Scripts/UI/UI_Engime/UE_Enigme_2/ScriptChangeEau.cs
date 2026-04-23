b4997a3d756ea18e5b3476ac57e4912a82b93258
vatea_RUI <vrui@gaming.tech>
2026-04-23 11:02:28 +0200

    [Update]
    [SucessFix[GestionCouronneFleur]]
    
    [Upgrate[Gestion Pot(miel/Néctar)+Fruits][Demander de tester cette mécanique]]
    
    [InProgress[Fix:_changeColorEthant]]

--- Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UE_Enigme_2/ScriptChangeEau.cs
+++ Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UE_Enigme_2/ScriptChangeEau.cs
@@ -3,37 +3,34 @@ using UnityEngine;
 
 public class ScriptChangeEau : MonoBehaviour
 {
-    private InteractionRunner _scriptActivation;
-    private BoxCollider boxCollider;
+    #region Variable
+    private float _lerpColorFloat=0;
     private Material _material;
-    //private SpriteRenderer _selfSprite;
-    //private int _nombreNuage;
-    private float _lerpColorFloat;
-
+    #endregion
+    #region Methode Variable
     void Start()
     {
-        //_selfSprite = GetComponent<SpriteRenderer>();
-        //_nombreNuage = 0;
-        _material = GetComponent<Material>();
-        _scriptActivation = GetComponent<InteractionRunner>();
-        boxCollider= GetComponent<BoxCollider>();
+        _material = GetComponentInChildren<SpriteRenderer>().material;
         Invoke("_DisableInteractionRunner", 0.1f);
-        _lerpColorFloat = 0;
-
+        _material.SetColor("BaseColor", Color.green);
     }
-
+    #endregion
 
     public void ChangeColor()
     {
         StartCoroutine(IEChangeColor());
+        Debug.Log("C'est Good, sa devrais changer la couleur de l'étant");
     }
-
+    public void printStatut()
+    {
+        Debug.Log("Tourne Banddéla");
+    }
     private IEnumerator IEChangeColor()
     {
         _lerpColorFloat = 0;
 
-        Color _colorOrigine = new Color(140.0f/ 255.0f,166.0f/255.0f, 163.0f / 255.0f, 1f);
-        Color _colorChange = new Color(230.0f / 255.0f, 255.0f / 255.0f, 168.0f/255.0f, 1f);
+        Color _colorOrigine = new Color(46.0f / 255.0f, 123.0f / 255.0f, 197.0f / 255.0f, 1f);
+        Color _colorChange = new Color(255.0f / 255.0f, 170.0f / 255.0f, 103.0f / 255.0f, 1f);
         Color _changingShadering;
         while (_lerpColorFloat < 1)
         {
@@ -41,11 +38,12 @@ public class ScriptChangeEau : MonoBehaviour
             //_selfSprite.color = Color.Lerp(Color.white, Color.yellow, _lerpColorFloat);
             //_selfSprite.color = Color.Lerp(_colorOrigine, _colorChange, _lerpColorFloat);
             _changingShadering = Color.Lerp(_colorOrigine, _colorChange, _lerpColorFloat);
-            _material.SetColor("BaseColor", _changingShadering);
+            if(_material!=null)_material.SetColor("BaseColor", _changingShadering);
+            if(_material!=null)_material.SetColor("_BaseColor", _changingShadering);
             Debug.Log("<color=green> Success</color>" + "ColorMaterial: " + _changingShadering);
             yield return new WaitForSeconds(0.1f);
         }
-        _scriptActivation.enabled= true;
-        boxCollider.enabled= true;
+        //_scriptActivation.enabled= true;
+        //_boxCollider.enabled= true;
     }
 }
