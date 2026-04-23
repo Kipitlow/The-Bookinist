80147414c4025b221e58f5252bcb5ee2f3ce7512
poplouf <pferran@gaming.tech>
2026-04-23 15:07:17 +0200

    [EDIT] fix balance

--- Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UI_Egnime_1/ScriptBalance.cs
+++ Bookinist/Assets/_Project/Scripts/UI/UI_Engime/UI_Egnime_1/ScriptBalance.cs
@@ -426,6 +426,9 @@ public class ScriptBalance : MonoBehaviour
         }
 
         _textValue.text = $"{_currentWeight} / 100";
+
+        if (_currentWeight >  0) _resetBalance.SetActive(true);
+        else _resetBalance.SetActive(false);
     }
 
     #endregion
