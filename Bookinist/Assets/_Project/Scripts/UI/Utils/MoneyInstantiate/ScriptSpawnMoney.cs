d0006e2b64ae5821f5bd9896d27e8574fec86366
vatea_RUI <vrui@gaming.tech>
2026-04-24 13:31:43 +0200

    [Final] Demande Merge
    -[Prefable]@GestionDropMoney
    -[Prefable]MoneyAffichage
    -[Scrip]ScriptMoneyShop
    -[Scrip]ScriptSpawnMoney
    
    C'est changement servirant pour animer le transfer d'argent/Gemme/Plume.

--- Bookinist/Assets/_Project/Scripts/UI/Utils/MoneyInstantiate/ScriptSpawnMoney.cs
+++ Bookinist/Assets/_Project/Scripts/UI/Utils/MoneyInstantiate/ScriptSpawnMoney.cs
@@ -1,12 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 
-enum _makeChoiceSprite { Money, Gemme, Plume }
+public enum _makeChoiceSprite { Money, Gemme, Plume }
 
+[System.Serializable]
 public class typeMoney
 {
-    
+    [Header("SetCurrent")]
+    public int currentMoney;
+    public int nombreInstanceMoney;
+    public _makeChoiceSprite choiceSprite;
+    public Transform targetSpawn;
 }
 
 public class ScriptSpawnMoney : MonoBehaviour
@@ -15,76 +21,73 @@ public class ScriptSpawnMoney : MonoBehaviour
     [Header("SetupPrefable")]
     [SerializeField] private GameObject _prefableMoney;
     [SerializeField] private Sprite[] _allSprite;
-    [SerializeField] private _makeChoiceSprite _choiceSprite;
+    [SerializeField] private List<typeMoney> _typeMoney=new();
 
-    [Header("SetCurrent")]
-    [SerializeField] private int _currentMoney;
-    private int _nombreInstanceMoney;
     #endregion
     #region Method Unity
     private void Start()
     {
-        switch (_choiceSprite)
-        {
-            case _makeChoiceSprite.Money: _nombreInstanceMoney = 10; break;
-            case _makeChoiceSprite.Gemme: _nombreInstanceMoney = 8; break;
-            case _makeChoiceSprite.Plume: _nombreInstanceMoney = 5; break;
-        }
+        PlaySpawnMoney();
+    }
+    public void PlaySpawnMoney()
+    {
         StartCoroutine(_instantiateMoney());
     }
     #endregion
     #region Unity
     private IEnumerator _instantiateMoney()
     {
-        int i = 0;
-        while(i< _nombreInstanceMoney)
+        foreach (typeMoney _var in _typeMoney)
         {
-            switch(_choiceSprite)
+            int i = 0;
+            while (i < _var.nombreInstanceMoney)
             {
-                case _makeChoiceSprite.Money:
-                    foreach(Sprite _sprite in _allSprite)
-                    {
-                        if (_sprite.name == "Franc")
+                switch (_var.choiceSprite)
+                {
+                    case _makeChoiceSprite.Money:
+                        foreach (Sprite _sprite in _allSprite)
                         {
-                            GameObject _object = Instantiate(_prefableMoney, this.transform.position, Quaternion.identity);
-                            ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
-                            script.targetTransform = GameObject.Find("SoftMoneyImage").GetComponent<RectTransform>();
-                            _object.GetComponent<SpriteRenderer>().sprite = _sprite;
-                            script.play(_currentMoney, _sprite.name);
+                            if (_sprite.name == "Franc")
+                            {
+                                GameObject _object = Instantiate(_prefableMoney, _var.targetSpawn.position, Quaternion.identity);
+                                ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
+                                script.targetTransform = GameObject.Find("SoftMoneyImage").GetComponent<RectTransform>();
+                                _object.GetComponent<SpriteRenderer>().sprite = _sprite;
+                                script.play(_var.currentMoney, _sprite.name);
+                            }
                         }
-                    }
-                    break;
-                case _makeChoiceSprite.Gemme:
-                    foreach (Sprite _sprite in _allSprite)
-                    {
-                        if (_sprite.name == "Gemme")
+                        break;
+                    case _makeChoiceSprite.Gemme:
+                        foreach (Sprite _sprite in _allSprite)
                         {
-                            GameObject _object = Instantiate(_prefableMoney, this.transform.position, Quaternion.identity);
-                            ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
-                            script.targetTransform = GameObject.Find("HardMoneyImage").GetComponent<RectTransform>();
-                            _object.GetComponent<SpriteRenderer>().sprite = _sprite;
-                            script.play(_currentMoney, _sprite.name);
+                            if (_sprite.name == "Gemme")
+                            {
+                                GameObject _object = Instantiate(_prefableMoney, _var.targetSpawn.position, Quaternion.identity);
+                                ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
+                                script.targetTransform = GameObject.Find("HardMoneyImage").GetComponent<RectTransform>();
+                                _object.GetComponent<SpriteRenderer>().sprite = _sprite;
+                                script.play(_var.currentMoney, _sprite.name);
+                            }
                         }
-                    }
-                    break;
-                case _makeChoiceSprite.Plume:
-                    foreach (Sprite _sprite in _allSprite)
-                    {
-                        if (_sprite.name == "Plume")
+                        break;
+                    case _makeChoiceSprite.Plume:
+                        foreach (Sprite _sprite in _allSprite)
                         {
-                            GameObject _object = Instantiate(_prefableMoney, this.transform.position, Quaternion.identity);
-                            ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
-                            script.targetTransform = GameObject.Find("IDunnoWhatsthis").GetComponent<RectTransform>();
-                            _object.GetComponent<SpriteRenderer>().sprite = _sprite;
-                            script.play(_currentMoney, _sprite.name);
+                            if (_sprite.name == "Plume")
+                            {
+                                GameObject _object = Instantiate(_prefableMoney, _var.targetSpawn.position, Quaternion.identity);
+                                ScriptMoneyShop script = _object.GetComponent<ScriptMoneyShop>();
+                                script.targetTransform = GameObject.Find("IDunnoWhatsthis").GetComponent<RectTransform>();
+                                _object.GetComponent<SpriteRenderer>().sprite = _sprite;
+                                script.play(_var.currentMoney, _sprite.name);
+                            }
                         }
-                    }
-                    break;
+                        break;
+                }
+                i++;
+                yield return new WaitForSeconds(0.05f);
             }
-            i++;
-            yield return new WaitForSeconds(0.05f);
         }
-
     }
     #endregion
 }
