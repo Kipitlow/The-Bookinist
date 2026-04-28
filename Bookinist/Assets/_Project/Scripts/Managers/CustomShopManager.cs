79f4a0b04ab51ed3587d4a0292e57efc1f746013
Enzo <130132864+Nyevius@users.noreply.github.com>
2026-04-28 13:10:32 +0200

    [EDIT] Change prefab struct

--- Bookinist/Assets/_Project/Scripts/Managers/CustomShopManager.cs
+++ Bookinist/Assets/_Project/Scripts/Managers/CustomShopManager.cs
@@ -131,7 +131,7 @@ public class CustomShopManager : MonoBehaviour
         //GameObject button = Instantiate(_buttonPrefab, _horizontalPanels[remapped].transform);
         GameObject button = Instantiate(_buttonPrefab, _horizontalPanels[viewIndex].transform);
 
-        Image buttonImage = button.GetComponent<Image>();
+        Image buttonImage = button.GetComponentInChildren<Image>();
         if (buttonImage != null && data.icon != null)
             buttonImage.sprite = data.icon;
 
