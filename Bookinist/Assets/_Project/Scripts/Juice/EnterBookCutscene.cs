6b2f952132bd640beeca4ec0ad12c8d81773d9bc
Nodeions <ehamida@gaming.tech>
2026-04-29 12:02:14 +0200

    [FIX]

--- Bookinist/Assets/_Project/Scripts/Juice/EnterBookCutscene.cs
+++ Bookinist/Assets/_Project/Scripts/Juice/EnterBookCutscene.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -10,11 +9,17 @@ public class EnterBookCutscene : MonoBehaviour
     [SerializeField] private TimelineAsset _loadTimeline;
 
     [SerializeField] private TimelineAsset _enterTimeline;
+    [SerializeField] private GameObject _onBoardingPanelToDeactivate;
+    [SerializeField] private GameObject _hand;
 
     public void EnterBook(bool enterBook)
     {
         if (enterBook)
+        {
+            _onBoardingPanelToDeactivate.SetActive(false);
+            _hand.SetActive(false);
             _sceneStarter.playableAsset = _enterTimeline;
+        }
         else
             _sceneStarter.playableAsset = _loadTimeline;
     }
