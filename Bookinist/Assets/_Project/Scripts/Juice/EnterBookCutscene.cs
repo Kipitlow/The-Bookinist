10ab871aabee1de0ec6ecf0273ebf43dae858212
Nodeions <ehamida@gaming.tech>
2026-04-29 11:34:41 +0200

    [FIX] Name branch brooooo

--- Bookinist/Assets/_Project/Scripts/Juice/EnterBookCutscene.cs
+++ Bookinist/Assets/_Project/Scripts/Juice/EnterBookCutscene.cs
@@ -1,4 +1,3 @@
-using UnityEditor.Timeline.Actions;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -11,10 +10,17 @@ public class EnterBookCutscene : MonoBehaviour
 
     [SerializeField] private TimelineAsset _enterTimeline;
 
+    [SerializeField] private GameObject _handToDisable;
+    [SerializeField] private GameObject _onboardingPanelToDisable;
+
     public void EnterBook(bool enterBook)
     {
         if (enterBook)
+        {
+            _handToDisable.SetActive(false);
+            _onboardingPanelToDisable.SetActive(false);
             _sceneStarter.playableAsset = _enterTimeline;
+        }
         else
             _sceneStarter.playableAsset = _loadTimeline;
     }
