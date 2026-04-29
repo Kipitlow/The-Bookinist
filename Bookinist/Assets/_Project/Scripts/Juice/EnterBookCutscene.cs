9bf94258412e389e7205faed0a538ea6e2d18b92
Amelia <147034763+DivineVoices@users.noreply.github.com>
2026-04-29 09:26:01 +0200

    [???] Jsp

---
+++ Bookinist/Assets/_Project/Scripts/Juice/EnterBookCutscene.cs
@@ -0,0 +1,26 @@
+using UnityEditor.Timeline.Actions;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class EnterBookCutscene : MonoBehaviour
+{
+    [SerializeField] private PlayableDirector _sceneStarter;
+
+    [SerializeField] private TimelineAsset _loadTimeline;
+
+    [SerializeField] private TimelineAsset _enterTimeline;
+
+    private void Start()
+    {
+        EnterBook(false);
+    }
+
+    public void EnterBook(bool enterBook)
+    {
+        if (enterBook)
+            _sceneStarter.playableAsset = _enterTimeline;
+        else
+            _sceneStarter.playableAsset = _loadTimeline;
+    }
+}
